ff96e5013ab2a8aaeaf168a37b89f393c5f9f5e4
deathcap <deathcap@gmx.co.uk>
2014-01-04 15:43:13 -0800

    Move example JSON to index.html, easier to format

--- demo.js
+++ demo.js
@@ -2,46 +2,6 @@ var tellraw2dom = require('./');
 var ever = require('ever');
 
 var inputNode = document.getElementById('input');
-// based on a good example from http://www.minecraftforum.net/topic/1980545-snapshot-website-to-generate-tellraw-commands/page__st__20#entry25630884
-inputNode.appendChild(document.createTextNode(JSON.stringify(
-{
-    "text": "Hey... ",
-    "color": "white",
-    "extra": [
-        {
-            "text": "CLICK HERE",
-            "color": "red",
-            "bold": "true",
-            "italic": "false",
-            "underlined": "true",
-            "strikethrough": "false",
-            "obfuscated": "false",
-            "clickEvent": {
-                "action": "run_command",
-                "value": "/tellraw @p {text:\"Thanks :3\",color:green}"
-            }
-        },
-        {
-            "text": " IF YOU DARE",
-            "color": "red",
-            "bold": "true",
-            "italic": "false",
-            "underlined": "false",
-            "strikethrough": "false",
-            "obfuscated": "false"
-        },
-        {
-            "text": ". then try hovering here",
-            "color": "yellow",
-            "italic": true,
-            "hoverEvent": {
-              "action": "show_item",
-              "value": "{id:322}"
-            }
-        }
-    ]
-})));
-
 var outputNode = document.getElementById('output');
 var hoverNode = document.getElementById('hover');
 
