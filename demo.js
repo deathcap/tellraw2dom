5bfb39a60da8f453ddd7ac5fb929d445fa831d8d
deathcap <deathcap@gmx.co.uk>
2014-01-04 14:50:19 -0800

    Decode JSON

--- demo.js
+++ demo.js
@@ -4,23 +4,28 @@ var ever = require('ever');
 var inputNode = document.createElement('textarea');
 inputNode.setAttribute('cols', '100');
 inputNode.setAttribute('rows', '20');
+// good example from http://www.minecraftforum.net/topic/1980545-snapshot-website-to-generate-tellraw-commands/page__st__20#entry25630884
+inputNode.appendChild(document.createTextNode(('value', '{"text":"Hey... ","color":"white","extra":[{"text":"CLICK HERE","color":"red","bold":"true","italic":"false","underlined":"true","strikethrough":"false","obfuscated":"false","clickEvent":{"action":"run_command","value":"/tellraw @p {text:\\"Thanks :3\\",color:green}"}},{"text":" IF YOU DARE","color":"red","bold":"true","italic":"false","underlined":"false","strikethrough":"false","obfuscated":"false"}]}')));
 
 var outputNode = document.createElement('div');
 
-ever(document.body).on('keydown', function(ev) {
+var update = function(ev) {
   var inputText = inputNode.value;
 
   while(outputNode.firstChild)
     outputNode.removeChild(outputNode.firstChild);
 
-  outputNode.appendChild(tellraw2dom(inputText));
-});
+  var output = tellraw2dom(inputText);
 
+  if (output)
+    outputNode.appendChild(output);
+};
+
+ever(document.body).on('keydown', update);
 
 document.body.appendChild(inputNode);
 document.body.appendChild(document.createElement('br'));
 document.body.appendChild(outputNode);
 
-ever(document.body).on('load', function() { // TODO
-  inputNode.focus();
-});
+update();
+
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 
+function parseRaw(element, parentState) {
+  if (typeof element === 'string') {
+    return document.createTextNode(element);
+  } else {
+    // TODO
+    return document.createTextNode('TODO');
+  }
+}
+
 module.exports = function(raw) {
+  try {
+    var json = JSON.parse(raw);
+  } catch (error) {
+    console.log(raw);
+    return document.createTextNode('Invalid JSON: ' + error);
+  }
 
-  return document.createTextNode(raw);
-  //var json = JSON.parse(raw); // TODO
+  return parseRaw(json);
 
+  // references:
+  // http://ezekielelin.com/minecraft/tellraw/
+  // https://github.com/deathcap/node-minecraft-protocol/blob/986cf0af918768e98ec6b95a9dfcab46f5204e5e/examples/client_chat.js#L116
 }
