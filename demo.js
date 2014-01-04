e117513f2319629ffec21d7212f4d8f3977bbfc1
deathcap <deathcap@gmx.co.uk>
2014-01-04 15:27:21 -0800

    Reformat JSON

--- demo.js
+++ demo.js
@@ -4,8 +4,36 @@ var ever = require('ever');
 var inputNode = document.createElement('textarea');
 inputNode.setAttribute('cols', '100');
 inputNode.setAttribute('rows', '20');
-// good example from http://www.minecraftforum.net/topic/1980545-snapshot-website-to-generate-tellraw-commands/page__st__20#entry25630884
-inputNode.appendChild(document.createTextNode(('value', '{"text":"Hey... ","color":"white","extra":[{"text":"CLICK HERE","color":"red","bold":"true","italic":"false","underlined":"true","strikethrough":"false","obfuscated":"false","clickEvent":{"action":"run_command","value":"/tellraw @p {text:\\"Thanks :3\\",color:green}"}},{"text":" IF YOU DARE","color":"red","bold":"true","italic":"false","underlined":"false","strikethrough":"false","obfuscated":"false"}]}')));
+// based on a good example from http://www.minecraftforum.net/topic/1980545-snapshot-website-to-generate-tellraw-commands/page__st__20#entry25630884
+inputNode.appendChild(document.createTextNode(JSON.stringify(
+{
+    "text": "Hey... ",
+    "color": "white",
+    "extra": [
+        {
+            "text": "CLICK HERE",
+            "color": "red",
+            "bold": "true",
+            "italic": "false",
+            "underlined": "true",
+            "strikethrough": "false",
+            "obfuscated": "false",
+            "clickEvent": {
+                "action": "run_command",
+                "value": "/tellraw @p {text:\"Thanks :3\",color:green}"
+            }
+        },
+        {
+            "text": " IF YOU DARE",
+            "color": "red",
+            "bold": "true",
+            "italic": "false",
+            "underlined": "false",
+            "strikethrough": "false",
+            "obfuscated": "false"
+        }
+    ]
+})));
 
 var outputNode = document.createElement('div');
 outputNode.setAttribute('style', 'background: black;');
